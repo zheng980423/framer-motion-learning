0dab1cbb2d663fc339b7a8a34a1b405031dfe0d1
zwf <zwf19980423@gmail.com>
2021-04-05 13:24:23 +0800

    hover state

--- src/components/Base.js
+++ src/components/Base.js
@@ -16,9 +16,14 @@ const Base = ({ addBase, pizza }) => {
         {bases.map(base => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
+              key={base}
+              onClick={() => addBase(base)}
+            >
               <span className={spanClass}>{base}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
@@ -31,7 +36,15 @@ const Base = ({ addBase, pizza }) => {
           className="next"
         >
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: '0px 0px 8px 0px rgb(255,255,255)',
+                boxShadow: '0px 0px 8px 0px rgb(255,255,255)',
+              }}
+            >
+              Next
+            </motion.button>
           </Link>
         </motion.div>
       )}
